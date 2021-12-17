cc7347c28326622c413098412e0f65020e75205e
chakib2002 <Abdelmalek.feddal@worldlearningalgeria.org>
2021-12-17 21:02:11 +0100

    small changes

--- components/Center.js
+++ components/Center.js
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react"
 function Center() {
     const {data : session }= useSession()
     return (
-        <div className="flex flex-grow">
+        <div className="flex-grow">
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-black space-x-3
                 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
@@ -16,7 +16,7 @@ function Center() {
                     <ChevronDownIcon className="h-5 w-5"/>
                 </div>
             </header>
-            <section className={`flex items-end w-full space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}>
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}>
                 {/* <img src="" alt="" /> */}
             </section>
         </div>
